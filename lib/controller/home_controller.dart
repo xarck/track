c85090652ced1a0b05c20e646596ccd474f73860
iamashking <iamashking>
2021-11-19 18:33:21 +0530

    initialized

---
+++ lib/controller/home_controller.dart
@@ -0,0 +1,7 @@
+class HomeController {
+  List data = [];
+
+  void fetchUserdata() {
+    data = [];
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:track/views/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Track',
+      theme: ThemeData(
+        fontFamily: "Proxima",
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
