946035df5b5a3ab435b26fb3ef0848bc92b1a3c2
iamashking <iamashking>
2021-11-22 17:28:26 +0530

    test

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:track/models/album_model.dart';
 import 'package:track/models/artist_model.dart';
-import 'package:track/models/attr_model.dart';
 import 'package:track/models/track_model.dart';
 
 class HomeController extends GetxController {
@@ -18,9 +17,11 @@ class HomeController extends GetxController {
         'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=$username&api_key=36b538790ba372126b3fc9447f1120c3&format=json'));
     var trackResponse = await http.get(Uri.parse(
         'http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=$username&api_key=36b538790ba372126b3fc9447f1120c3&format=json'));
-
-    json
-        .decode(trackResponse.body)['toptracks']
-        .map((track) => topTracks.add(TrackModel.fromJson(track)));
+    print(json.decode(trackResponse.body)['toptracks']['track']);
+    json.decode(trackResponse.body)['toptracks']['track'].map((track) {
+      topTracks.add(TrackModel.fromMap(track));
+      print('2');
+    });
+    print(topTracks.length);
   }
 }
